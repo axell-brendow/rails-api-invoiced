e4c44b1a1845b8fc0e79385cf6658eeaff926c27
axell-brendow <breno.axel@gmail.com>
2020-11-06 10:39:22 -0300

    Setup tests and SimpleCov

--- test/controllers/v1/contacts_controller_test.rb
+++ test/controllers/v1/contacts_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class V1::ContactsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @contact = contacts(:one)
+  end
+
+  test 'index' do
+    get v1_contacts_url
+    assert_response :ok
+  end
+
+  test 'create' do
+    assert_difference('Contact.count') do
+      post v1_contacts_url, params: {
+        contact: {
+          first_name: @contact.first_name,
+          last_name: @contact.last_name,
+          email: @contact.email
+        }
+      }
+    end
+  end
+
+  test 'destroy' do
+    assert_difference('Contact.count', -1) do
+      delete v1_contact_url(@contact)
+    end
+  end
 end
