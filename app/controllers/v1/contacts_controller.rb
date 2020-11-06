dd1d133a9e4e6ea5c56ad3b985ca6980cc0bc9fa
axell-brendow <breno.axel@gmail.com>
2020-11-06 10:37:26 -0300

    Create Contacts Controller

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -1,2 +1,27 @@
-class V1::ContactsController < ApplicationController
+class V1::ContactsController < V1::ApplicationController
+  def index
+    @contacts = Contact.all
+
+    render json: @contacts, status: :ok
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    @contact.save
+
+    render json: @contact, status: :created
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    head(:no_content)
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email)
+  end
 end
